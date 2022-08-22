b07730b85b5faaa7daa0a981d0b793ac04436b4b
ch4itu <63451145+ch4itu@users.noreply.github.com>
2022-08-22 11:54:00 +0530

    Add files via upload

---
+++ mBlogger.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: The Unlicense
+
+// Solidity program for simple micro blogging
+pragma solidity ^0.4.21;
+
+contract mBlogger {
+  
+    // Declaring state variables
+    string public mPost;
+
+    // Declaring an event 
+    event microPost(string blogPost);
+  
+    // Emitter function for saving the post
+    function getPost(string bPost) public {
+        emit microPost(bPost); 
+       mPost = bPost;
+    }
+}
